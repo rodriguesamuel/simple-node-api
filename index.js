d9860077718b8160ce3ae8975eb0b75f08361cb9
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-28 15:58:33 -0300

    new edpoints added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var fs = require('fs');
+const fs = require('fs');
 
 // json file with the data
 var data = fs.readFileSync('projects.json');
 var project = fs.readFileSync('project.json');
 var phasesJson = fs.readFileSync('phases.json');
 var historyLogJson = fs.readFileSync('historyLog.json');
+var processJson = fs.readFileSync('processes.json');
+var userJson = fs.readFileSync('users.json');
+var installationsJson = fs.readFileSync('installations.json');
+var projTypesJson = fs.readFileSync('projTypes.json');
 
 var elements = JSON.parse(data);
 var projectData = JSON.parse(project);
 var phasesData = JSON.parse(phasesJson);
 var historyLogData = JSON.parse(historyLogJson);
+var processData = JSON.parse(processJson);
+var userData = JSON.parse(userJson);
+var instalationsData = JSON.parse(installationsJson);
+var projTypesData = JSON.parse(projTypesJson);
 const express = require("express");
 const app = express();
 
@@ -17,7 +25,7 @@ const app = express();
 const cors = require('cors');
 
 app.listen(3000,
-    () => console.log("Server Start at the Port"));
+    () => console.log("Server Start at the Port: 3000"));
 
 app.use(express.static('public'));
 app.use(cors());
@@ -45,5 +53,55 @@ app.get('/project-history/:id/', getHistory);
 function getHistory(request, response) {
     // Returns all phases
     response.send(historyLogData);
+}
+
+app.get('/processes', getProcess);
+function getProcess(request, response) {
+    // Returns all phases
+    console.log("in getProcess");
+    response.send(processData);
+}
+
+app.post('/project-save', saveProject);
+function saveProject(request, response) {
+    // Returns all phases
+    response.send(project);
+}
+
+
+app.get('/users', getUsers);
+function getUsers(request, response) {
+    // Returns all phases
+    console.log("in getUsers");
+
+    const delay = 3000;
+    setTimeout(function() {
+        response.send(userData);
+    }, delay);
+
+}
+
+app.get('/installations', getInstallations);
+function getInstallations(request, response) {
+    // Returns all phases
+    console.log("in getInstallations");
+
+    const delay = 3000;
+    setTimeout(function() {
+        response.send(instalationsData);
+    }, delay);
+
+}
+
+app.get('/project-types', getProjTypes);
+function getProjTypes(request, response) {
+    // Returns all phases
+    console.log("in getProjTypes");
+
+    const delay = 3000;
+    setTimeout(function() {
+        response.send(projTypesData);
+    }, delay);
+
 }
 
